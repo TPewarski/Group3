ab6df78dcb1c9711e8b019f520d3ddc8f0dd3fc9
Tyler Pewarski <tylerpewarski@Tylers-MacBook-Pro.local>
2015-06-09 18:07:53 -0400

    need help with bugfix

---
+++ browser/js/cartState/cart.js
@@ -0,0 +1,14 @@
+app.config(function($stateProvider){
+	$stateProvider.state('cart', {
+		url: '/cart',
+		controller: 'CartController',
+		templateUrl: 'js/cartState/cart.html'
+	})
+})
+
+app.controller('CartController', function($scope, cartFactory){
+	console.log("hello WOrld")
+    $scope.items = cartFactory.items
+
+
+})
---
+++ browser/js/common/factories/cartFactory.js
@@ -0,0 +1,14 @@
+app.factory('cartFactory', function($http){
+	return {
+			// maybe do it in this format?? {product: {}, quantity: num}
+		items: [{product: {
+            name: "Dire Wolf",
+            description: "An unusually large and intelligent species of wolf. Great gift for children of all ages.",
+            price: 250000,
+            inventoryQuantity: 12,
+            categories: ['Animal', 'Dire Wolf'],
+            imgPath: "https://kaylahoailinh.files.wordpress.com/2014/10/wallpaper-hd-dire-wolf-hd-cool-7-hd-wallpapers.jpeg?w=726&h=203"
+        }, quantity: 1}]
+		//userId or session ID
+	}
+})
