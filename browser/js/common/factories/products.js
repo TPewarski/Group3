e2bb4735cdb88d76134ea657f98974f0003327cd
Tyler Pewarski <tylerpewarski@Tylers-MacBook-Pro.local>
2015-06-09 15:48:13 -0400

    double-clicking in all products view will redirect to single product view for that item

--- browser/js/common/factories/products.js
+++ browser/js/common/factories/products.js
@@ -1,5 +1,14 @@
 app.factory('productsFactory', function($http){
 	return {
+		singleProduct: {
+            name: "Dire Wolf",
+            description: "An unusually large and intelligent species of wolf. Great gift for children of all ages.",
+            price: 250000,
+            inventoryQuantity: 12,
+            categories: ['Animal', 'Dire Wolf'],
+            imgPath: "https://kaylahoailinh.files.wordpress.com/2014/10/wallpaper-hd-dire-wolf-hd-cool-7-hd-wallpapers.jpeg?w=726&h=203"
+        },
+
 		getAllProducts : function() {
 			console.log("Hit get from factory")
 			return $http.get('/api/GETproducts/allproducts').
