bcb96d2bf9d044d5c23fd10e9db47d5569d89709
Jovi Chokor <jomonavi@gmail.com>
2015-06-12 19:07:44 -0400

    search works, needs to be cleaned up tho

--- browser/js/singleProductState/singleProduct.js
+++ browser/js/singleProductState/singleProduct.js
@@ -7,16 +7,15 @@ app.config(function($stateProvider){
 });
 
 app.controller('SingleProductController', function($scope, productsFactory, cartFactory, $stateParams, $state){
-	console.log("i has it", $scope.products);
 	productsFactory.getAllProducts().then(function(productsArray) {
-		$scope.products = productsArray.data;
+		$scope.products = productsArray;
 
 		$scope.products.forEach(function(aProd) {
 			if(aProd._id === $stateParams.theID) {
 				$scope.anItem = aProd;
 			}
 		});	
-		console.log("an item", $scope.anItem);
+		// console.log("an item", $scope.anItem);
 	});
 
 	$scope.addToCart = function(){
