a8a5aed227390d8719dd769833c84a0c443b440b
Jovi Chokor <jomonavi@gmail.com>
2015-06-15 19:43:12 -0400

    user views cleaned up

---
+++ browser/js/common/directives/account-settings/accountSettings.js
@@ -0,0 +1,9 @@
+// app.directive('userSettings', function() {
+// 	return {
+// 		restrict: 'E',
+// 		templateUrl: 'js/common/directives/account-settings/accountSettings.html',
+// 		link: function (scope, ele, attr){
+			
+// 		}
+// 	};
+// });
