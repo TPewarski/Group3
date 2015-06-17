c16fbf58729c9ab56deb3d4f4f3ce5be0976d4a5
Ming Lei <mingus718@gmail.com>
2015-06-16 20:32:29 -0400

    bugfix for beta 3.42

--- browser/js/user/userprofile.js
+++ browser/js/user/userprofile.js
@@ -22,12 +22,8 @@ app.controller('UserController', function($scope, $state, AuthService, UserFacto
     $scope.showSettings = false;
     $scope.isAnAdmin = false;
     $scope.showUsers = false;
-<<<<<<< HEAD
     $scope.adminOrders = false;
-=======
     $scope.orders= null;
-    
->>>>>>> b9e7b0c07795ac6d55e5a096e0399dbf30753fe9
     AuthService.getLoggedInUser().then(function(user) {
         $scope.theUser = user;
         $scope.isAnAdmin = user.isAdmin;
